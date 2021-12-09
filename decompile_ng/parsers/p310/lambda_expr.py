98e1b62c0d2408aecc0ab08dd6bb758cc132c444
rocky <rb@dustyfeet.com>
2021-12-08 21:29:17 -0500

    Remove unused (for now) lambda rules

--- decompile_ng/parsers/p310/lambda_expr.py
+++ decompile_ng/parsers/p310/lambda_expr.py
@@ -64,20 +64,10 @@ class Python310LambdaParser(Python310BaseParser):
 
         if_exp_lambda2     ::= and_parts return_lambda
                                return_lambda opt_lambda_marker
+        opt_lambda_marker  ::= LAMBDA_MARKER?
 
         if_exp_not_lambda2 ::= expr_pjit dom_start expr
                                RETURN_VALUE_LAMBDA bb_doms_end return_lambda
-        # No gone over...
-
-        return_if_lambda   ::= RETURN_END_IF_LAMBDA COME_FROM
-        return_if_lambda   ::= RETURN_END_IF_LAMBDA
-
-        if_exp_lambda      ::= expr_pjif expr return_if_lambda
-                               return_lambda LAMBDA_MARKER
-        if_exp_not_lambda  ::= expr POP_JUMP_IF_TRUE expr return_if_lambda
-                               return_lambda LAMBDA_MARKER
-        if_exp_dead_code   ::= return_lambda return_lambda
-        opt_lambda_marker  ::= LAMBDA_MARKER?
         """
 
     def p_bool_ops(self, args):
