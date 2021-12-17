b870f79a5ae8f621467293b8299d451c3f257bb6
rocky <rb@dustyfeet.com>
2021-12-16 23:34:05 -0500

    Add code to decompile just lambdas

--- decompile_ng/scanner.py
+++ decompile_ng/scanner.py
@@ -545,10 +545,10 @@ def get_scanner(version, is_pypy=False, show_asm=None):
 
 
 if __name__ == "__main__":
-    import inspect, decompile_ng
+    import inspect
 
     co = inspect.currentframe().f_code
     from xdis.version_info import PYTHON_VERSION_TRIPLE
 
-    scanner = get_scanner(decompile_ng.PYTHON_VERSION_TRIPLE, IS_PYPY, True)
+    scanner = get_scanner(PYTHON_VERSION_TRIPLE, IS_PYPY, True)
     tokens, customize = scanner.ingest(co, {}, show_asm="after")
