1eef427be62eda9ea79077b6ae2db206804670d6
rocky <rb@dustyfeet.com>
2022-01-01 17:42:58 -0500

    bump copyright

--- decompile_ng/scanners/scanner310base.py
+++ decompile_ng/scanners/scanner310base.py
@@ -1,4 +1,4 @@
-#  Copyright (c) 2015-2021 by Rocky Bernstein
+#  Copyright (c) 2015-2022 by Rocky Bernstein
 #  Copyright (c) 2005 by Dan Pascu <dan@windowmaker.org>
 #  Copyright (c) 2000-2002 by hartmut Goebel <h.goebel@crazy-compilers.com>
 #
--- decompile_ng/scanners/tok.py
+++ decompile_ng/scanners/tok.py
@@ -1,4 +1,4 @@
-#  Copyright (c) 2016-2020 by Rocky Bernstein
+#  Copyright (c) 2016-2020, 2022 by Rocky Bernstein
 #  Copyright (c) 2000-2002 by hartmut Goebel <h.goebel@crazy-compilers.com>
 #  Copyright (c) 1999 John Aycock
 #
