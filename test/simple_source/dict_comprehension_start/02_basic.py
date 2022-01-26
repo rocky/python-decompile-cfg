7d9418568827a3426176d1e5e917681b984884e4
rocky <rb@dustyfeet.com>
2022-01-26 12:49:29 -0500

    Beef up dict-comprehension tests

--- test/simple_source/dict_comprehension_start/02_basic.py
+++ test/simple_source/dict_comprehension_start/02_basic.py
@@ -12,3 +12,56 @@
  in __file__
  for i
  in t}
+
+
+# line 36 of from Python 3.8 spacy/tests/regression/test_issue4190.py
+# The problem is in "if_not"
+{
+    k: v
+    for k, v in
+    __file__
+    if not
+    (k
+     and v)
+    }
+
+# Note this is the same as" if not .. if ...
+{
+    k: v
+    for k, v in
+    __file__
+    if not k
+    if v
+    }
+
+# line 39 or 3.8.12 dateutil/zoneinfo/__init__.py
+# problem was handling "or" in comprehension
+{
+ zl.name: zl.linkname
+ for
+ zl in __file__
+ if
+    zl.name or zl.foo}
+
+# Line 39 of 3.8.12 pre_commit/git.py
+# problem was handling " not (... or.. )" in comprehension
+{k: v for k, v in __file__
+ if not k.startswith('GIT_') or
+ k in {
+     'GIT_EXEC_PATH'}
+}
+
+
+# Line 1866 of 3.8.12 test/test_coroutines.py
+async def run_dict():
+    return {i + 1: i + 2 async for i in [10, 20]}
+
+# Line 19266 of 3.8.12 test/test_coroutines.py
+
+async def run_dict2():
+    {i:
+     i
+     async
+     for i in range(5)
+     if 0 <
+     i < 4}
