724831a5bc52ae69082ba4e44509eaeb3022d790
rocky <rb@dustyfeet.com>
2022-02-11 19:42:14 -0500

    Segregate and beef up tests
    More coverage over more and smaller tests
    
    For now we are isolating list comprensions which seem
    to pull in the entire grammar rather than just the lambdas

---
+++ test/simple_source/lambda_start/expression/01_misc_exprs.py
@@ -0,0 +1,14 @@
+# FIXME: We don't have statements in yet.
+# These are an accumulation of lambda expressions involving logical operations.
+# They were culled from all lambda's on my disk under Python 3.8.
+
+# NOTE: Formatting may be weird because we want the additional line numbers
+# in debugging.
+
+# fmt: off
+
+# Unary operators
+lambda a: -a
+lambda b: +b
+lambda c: ~c
+lambda d: d[1:3]
