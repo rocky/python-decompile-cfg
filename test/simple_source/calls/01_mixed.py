5de99581f33064c2f0dc57364838b57ef481b960
rocky <rb@dustyfeet.com>
2022-02-23 14:20:35 -0500

    Test more exotic calls

--- test/simple_source/calls/01_mixed.py
+++ test/simple_source/calls/01_mixed.py
@@ -19,3 +19,34 @@ assert min(*[1,2,3], *[0, 10, 20]) == 0
 assert sorted([1,2], reverse=True, key=None)
 
 assert (lambda x: x.keys())(dict(zip(range(4), range(4)))) == set(range(4))
+
+
+# Adapted from decompyle's test_appyEquiv.py
+
+def kwfunc(**kwargs):
+    return(kwargs.items())
+
+
+def argsfunc(*args):
+    return(args)
+
+
+def argsfunc(*args):
+    return(args)
+
+def no_apply(*args, **kwargs):
+    return args, kwargs
+
+def args_kwargs_test(*args, **kwargs):
+    assert argsfunc(34) == (34,)
+    assert argsfunc(*args) == (1, 2, 4, 8)
+    assert argsfunc(34, *args) == (34, 1, 2, 4, 8)
+    assert not kwfunc(**{})
+    assert dict(kwfunc(x = 11, **{})) == {"x": 11}
+    assert no_apply(*args, **kwargs) == ((1, 2, 4, 8), {'a': 2, 'b': 3, 'c': 5})
+    assert no_apply(34, *args, **kwargs) == ((34, 1, 2, 4, 8), {'a': 2, 'b': 3, 'c': 5})
+    assert no_apply(x = 11, *args, **kwargs) == ((1, 2, 4, 8), {'x': 11, 'a': 2, 'b': 3, 'c': 5})
+    # assert no_apply(34, x = 11, *args, **kwargs) == ((34, 1, 2, 4, 8), {'x': 11, 'a': 2, 'b': 3, 'c': 5})
+    # assert no_apply(42, 34, x = 11, *args, **kwargs) == ((42, 34, 1, 2, 4, 8), {'x': 11, 'a': 2, 'b': 3, 'c': 5})
+
+args_kwargs_test(1, 2, 4, 8, a = 2, b = 3, c = 5)
