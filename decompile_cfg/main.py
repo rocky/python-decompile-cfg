518b31b08cec078c9ec51ffbfb9dfcbbd89c5f3d
rocky <rb@dustyfeet.com>
2022-03-01 12:55:08 -0500

    Handle --verify option

--- decompile_cfg/main.py
+++ decompile_cfg/main.py
@@ -13,8 +13,11 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import datetime, py_compile, os, sys
-from typing import Any, Tuple
+import ast, datetime, py_compile, os, sys, traceback
+import os.path as osp
+import subprocess
+import tempfile
+from typing import Any, Optional, Tuple
 from xdis import iscode, load_module
 from xdis.version_info import version_tuple_to_str
 
@@ -30,17 +33,27 @@ from decompile_cfg.semantics.pysource import code_deparse, PARSER_DEFAULT_DEBUG
 from decompile_cfg.semantics.fragments import code_deparse as code_deparse_fragments
 from decompile_cfg.semantics.linemap import deparse_code_with_map
 
-
 def _get_outstream(outfile: str) -> Any:
-    dir = os.path.dirname(outfile)
+    dir = osp.dirname(outfile)
     failed_file = outfile + "_failed"
-    if os.path.exists(failed_file):
+    if osp.exists(failed_file):
         os.remove(failed_file)
     try:
         os.makedirs(dir)
     except OSError:
         pass
     return open(outfile, mode="w", encoding="utf-8")
+
+
+def syntax_check(filename: str) -> bool:
+    with open(filename) as f:
+        source = f.read()
+    valid = True
+    try:
+        ast.parse(source)
+    except SyntaxError:
+        valid = False
+    return valid
 
 
 def decompile(
@@ -242,7 +255,7 @@ def main(
     outfile=None,
     showasm=None,
     showast={},
-    do_verify=False,
+    do_verify=Optional[str],
     showgrammar=False,
     source_encoding=None,
     raise_on_error=False,
@@ -268,9 +281,9 @@ def main(
         compiled_files.append(compile_file(source_path))
 
     for filename in compiled_files:
-        infile = os.path.join(in_base, filename)
+        infile = osp.join(in_base, filename)
         # print("XXX", infile)
-        if not os.path.exists(infile):
+        if not osp.exists(infile):
             sys.stderr.write("File '%s' doesn't exist. Skipped\n" % infile)
             continue
 
@@ -283,14 +296,19 @@ def main(
         if outfile:  # outfile was given as parameter
             outstream = _get_outstream(outfile)
         elif out_base is None:
-            outstream = sys.stdout
+            out_base = tempfile.mkdtemp(prefix="py-dis-")
+            if do_verify and filename.endswith(".pyc"):
+                current_outfile = osp.join(out_base, filename[0:-1])
+                outstream = open(current_outfile, "w")
+            else:
+                outstream = sys.stdout
             if do_linemaps:
                 linemap_stream = sys.stdout
         else:
             if filename.endswith(".pyc"):
-                current_outfile = os.path.join(out_base, filename[0:-1])
+                current_outfile = osp.join(out_base, filename[0:-1])
             else:
-                current_outfile = os.path.join(out_base, filename) + "_dis"
+                current_outfile = osp.join(out_base, filename) + "_dis"
                 pass
             pass
 
@@ -300,7 +318,7 @@ def main(
 
         # Try to uncompile the input file
         try:
-            deparsed = decompile_file(
+            deparsed_objects = decompile_file(
                 infile,
                 outstream,
                 showasm,
@@ -311,9 +329,9 @@ def main(
                 do_fragments,
             )
             if do_fragments:
-                for d in deparsed:
+                for deparsed_object in deparsed_objects:
                     last_mod = None
-                    offsets = d.offsets
+                    offsets = deparsed_object.offsets
                     for e in sorted(
                         [k for k in offsets.keys() if isinstance(k[1], int)]
                     ):
@@ -322,12 +340,27 @@ def main(
                             outstream.write("%s\n%s\n%s\n" % (line, e[0], line))
                         last_mod = e[0]
                         info = offsets[e]
-                        extractInfo = d.extract_node_info(info)
+                        extractInfo = deparsed_object.extract_node_info(info)
                         outstream.write("%s" % info.node.format().strip() + "\n")
                         outstream.write(extractInfo.selectedLine + "\n")
                         outstream.write(extractInfo.markerLine + "\n\n")
                     pass
                 pass
+            if do_verify == "run":
+                for deparsed_object in deparsed_objects:
+                    from trepan.api import debug; debug()
+                    if PYTHON_VERSION_TRIPLE[:2] == deparsed_object.version:
+                        result = subprocess.run([sys.executable, deparsed_object.f.name], capture_output=True)
+                        valid = result.returncode == 0
+                    else:
+                        valid = syntax_check(deparsed_object.f.name)
+                    # sys.stderr.write(f"Ran {deparsed_object.f.name}\n")
+                    if valid:
+                        verify_failed_files += 1
+                        print(result.stderr.decode())
+                    output = result.stdout.decode()
+                    if output:
+                        print(output)
             tot_files += 1
         except (ValueError, SyntaxError, ParserError, pysource.SourceWalkerError) as e:
             sys.stdout.write("\n")
@@ -428,6 +461,8 @@ def status_msg(do_verify, tot_files, okay_files, failed_files, verify_failed_fil
             return "\n# decompile failed"
         elif verify_failed_files:
             return "\n# decompile run verification failed"
+        elif do_verify:
+            return "\n# Successfully decompiled and ran file"
         else:
             return "\n# Successfully decompiled file"
             pass
