ff0a594e470757094eb7f69d26aeee944776c916
rocky <rb@dustyfeet.com>
2022-03-24 23:22:20 -0400

    Add if .. else test

--- test/simple_source/stmts/05_if_assign.py
+++ test/simple_source/stmts/05_if_assign.py
@@ -1,1 +1,12 @@
-a = 'True' if __name__ == '__main__' else False
+"""This program is self-checking!"""
+
+# We use line breaks to assist marking bytecode sections
+# fmt: off
+a = (
+    "True" if (
+    __name__ == "__main__"
+    ) else (
+        False)
+    )
+
+assert a == "True"
