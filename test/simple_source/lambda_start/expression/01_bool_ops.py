cf774062d5067b58246b1584487342d816ccbb3b
rocky <rb@dustyfeet.com>
2023-07-29 09:40:55 -0400

    Start adding lambda bool opts tests

--- test/simple_source/lambda_start/expression/01_bool_ops.py
+++ test/simple_source/lambda_start/expression/01_bool_ops.py
@@ -1,24 +1,24 @@
 lambda a: not a
 
 lambda a, b: a and b
-lambda a, b, c: a and b and c
-lambda a, b, c, d:  a and b and c and d
+# lambda a, b, c: a and b and c
+# lambda a, b, c, d:  a and b and c and d
 
-lambda a, b: a or b
-lambda a, b, c: a or b or c
-lambda a, b, c, d:  a or b or c or d
+# lambda a, b: a or b
+# lambda a, b, c: a or b or c
+# lambda a, b, c, d:  a or b or c or d
 
 # Mixed and/or
-lambda a, b, c:  (a and b) or c
-lambda a, b, c:  a and (b or c)
+# lambda a, b, c:  (a and b) or c
+# lambda a, b, c:  a and (b or c)
 
-lambda a, b, c:  (a or b) and c
-lambda a, b, c:  a or (b and c)
+# lambda a, b, c:  (a or b) and c
+# lambda a, b, c:  a or (b and c)
 
 # Mixed and/not
 
-lambda a, b, c:  a and b and not c
-lambda a, b, c:  a and b or not c
+# lambda a, b, c:  a and b and not c
+# lambda a, b, c:  a and b or not c
 
-lambda a, b, c:  a or b and not c
-lambda a, b, c:  a or b or not c
+# lambda a, b, c:  a or b and not c
+# lambda a, b, c:  a or b or not c
