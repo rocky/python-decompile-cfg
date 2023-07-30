d376b05c8b458586b95281813d6697ddbaee5c7a
rocky <rb@dustyfeet.com>
2023-07-30 17:36:25 -0400

    Start call testing

--- test/simple_source/lambda_start/calls/01_fns.py
+++ test/simple_source/lambda_start/calls/01_fns.py
@@ -1,68 +1,39 @@
-# FIXME: We don't have statements in yet.
-# These are an accumulation of lambda expressions involving all of the varied kinds of
-# variable parameters.
-# They were culled from all lambda's on my disk under Python 3.8.
-
 # NOTE: Formatting may be weird because we want the additional line numbers
 # in debugging.
 
-# we can turn this into a self-checking program when we do.
-# """This program is self-checking!"""
-
-def func(*args, **kwargs):
-    pass
-
 # fmt: off
-lambda: globals()
-lambda: locals()
-lambda d=b'': 5
 
-lambda func, *args, **kw: 5
+# Here we test the various form of function signatures and funtion calls
 
-lambda func, *args, **kw: func(**kw)
+# No args
+lambda: 1
 
-lambda no_apply, *args, **kwargs: no_apply(*args, **kwargs)
-lambda no_apply, *args, **kwargs: no_apply(1, *args, **kwargs)
-lambda no_apply, *args, **kwargs: no_apply(1, 2, *args, **kwargs)
-lambda no_apply, *args, **kwargs: no_apply(x = 11, *args, **kwargs)
-lambda no_apply, *args, **kwargs: no_apply(34, x = 11, *args, **kwargs)
-lambda no_apply, *args, **kwargs: no_apply(42, 34, x = 11, *args, **kwargs)
-lambda func, *args, **kw: func(*args, **kw)
-lambda self, *args, **kw: func(self, *args, **kw)
+# positional args
+lambda a: a
+lambda a, b: a + b
+
+# default values
+lambda a, b=b'': a + b
+lambda s=b'': s
+
+lambda f, *args: f(args)
+lambda func, *args, **kw: func(args, *args, **kw)
+
+# lambda func, *args, **kw: func(**kw)
+
+# lambda no_apply, *args, **kwargs: no_apply(*args, **kwargs)
+# lambda no_apply, *args, **kwargs: no_apply(1, *args, **kwargs)
+# lambda no_apply, *args, **kwargs: no_apply(1, 2, *args, **kwargs)
+# lambda no_apply, *args, **kwargs: no_apply(x = 11, *args, **kwargs)
+# lambda no_apply, *args, **kwargs: no_apply(34, x = 11, *args, **kwargs)
+# lambda no_apply, *args, **kwargs: no_apply(42, 34, x = 11, *args, **kwargs)
+# lambda func, *args, **kw: func(*args, **kw)
+# lambda self, *args, **kw: func(self, *args, **kw)
 
 
-lambda func, *args, **kw: func(*args,
-                               **kw)
+# lambda func, *args, **kw: func(*args,
+#                                **kw)
 
-lambda self, *args, **kw: func(self,
-                               *args,
-                               **kw)
-
-
-# Numpy code has a rich source of lamddas
-lambda x: func(mapdomain(x,
-                         window,
-                         domain),
-               *args)
-
-lambda func, *args, **kw: func(*args,
-                               **kw)
-
-lambda self, *args, **kw: func(self,
-                               *args,
-                               **kw)
-
-lambda s: int(s or -999)
-
-lambda *paths: join(*((sep,)+paths))
-
-# From numpy/polynomial/chebyshev.py
-lambda x: func(
-    pu(x, window, domain),
-    *args)
-
-# From pint/registry.py
-lambda x, case_sensitive, values, eval_token: eval_token(
-    x,
-    case_sensitive=case_sensitive,
-    **values)
+# lambda self, *args, **kw: func(self,
+#                                *args,
+#                                **kw)
