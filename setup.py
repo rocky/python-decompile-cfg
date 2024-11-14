7d52ffdd0b137c08128d85e5d1a2ee144c67cd61
rocky <rb@dustyfeet.com>
2024-11-14 16:06:25 -0500

    Convert to using pyproject

--- setup.py
+++ setup.py
@@ -1,51 +1,9 @@
 #!/usr/bin/env python
-import sys
 
 """Setup script for the 'decompile-cfg' distribution."""
-
-SYS_VERSION = sys.version_info[0:2]
-if SYS_VERSION < (3, 8):
-    mess = f"\nThis package is not supported for Python version {sys.version[0:3]}."
-    mess += "\nFor earlier versions, use decompile3 or uncompyle6."
-    print(mess)
-    raise Exception(mess)
-elif (3, 8) <= SYS_VERSION[:2] != (3, 10):
-    mess = f"\nThis package does not decompile for Python version {sys.version[0:3]}, just 3.8 .. 3.9."
-    print(mess)
-
-from __pkginfo__ import (
-    author,
-    author_email,
-    install_requires,
-    license,
-    long_description,
-    classifiers,
-    entry_points,
-    modname,
-    py_modules,
-    short_desc,
-    __version__,
-    web,
-    zip_safe,
-)
 
 from setuptools import setup, find_packages
 
 setup(
-    author=author,
-    author_email=author_email,
-    classifiers=classifiers,
-    description=short_desc,
-    entry_points=entry_points,
-    install_requires=install_requires,
-    license=license,
-    long_description=long_description,
-    name=modname,
     packages=find_packages(),
-    py_modules=py_modules,
-    test_suite="nose.collector",
-    url=web,
-    tests_require=["nose>=1.0"],
-    version=__version__,
-    zip_safe=zip_safe,
 )
