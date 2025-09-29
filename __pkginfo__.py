97e647aa43e9fc438f94fe0ee0dbd76304b411d9
rocky <rb@dustyfeet.com>
2025-09-29 10:21:19 -0400

    Administrivia

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2018-2021, 2023 Rocky Bernstein <rocky@gnu.org>
+# Copyright (C) 2018-2025 Rocky Bernstein <rocky@gnu.org>
 #
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
@@ -12,9 +12,7 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-"""decompile-cfg packaging information"""
-
-import os.path as osp
+"""decompyle3 packaging information"""
 
 # To the extent possible we make this file look more like a
 # configuration file rather than code like setup.py. I find putting
@@ -23,9 +21,11 @@ import os.path as osp
 # less elegant than having it here with reduced code, albeit there
 # still is some room for improvement.
 
+import os.path as osp
+
 # Things that change more often go here.
 copyright = """
-Copyright (C) 2021-2023 Rocky Bernstein <rb@dustyfeet.com>.
+Copyright (C) 2015-2021, 2024-2025 Rocky Bernstein <rb@dustyfeet.com>.
 """
 
 classifiers = [
@@ -33,33 +33,31 @@ classifiers = [
     "Intended Audience :: Developers",
     "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
     "Operating System :: OS Independent",
+    "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
     "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: Implementation :: PyPy",
     "Topic :: Software Development :: Debuggers",
     "Topic :: Software Development :: Libraries :: Python Modules",
 ]
 
 # The rest in alphabetic order
-author = "Rocky Bernstein, Hartmut Goebel, John Aycock, and others"
+author = "Rocky Bernstein"
 author_email = "rb@dustyfeet.com"
 entry_points = {
     "console_scripts": [
-        "decompile-cfg=decompile_cfg.bin.decompile:main_bin",
-        "decompile-cfg-code=decompile_cfg.bin.decompile_code_type:main",
-        "decompile-cfg-tokens=decompile_cfg.bin.decompile_cfg_tokens:main",
+        "decompile-cfg=decompyle3.bin.decompile:main_bin",
+        "decompile-cfg-code=decompyle3.bin.decompile_code_type:main",
+        "decompile-cfg-tokens=decompyle3.bin.decompile_tokens:main",
     ]
 }
 ftp_url = None
-install_requires = [
-    "click",
-    "control_flow >= 0.9",
-    "spark-parser >= 1.8.9, < 1.9.1",
-    "xdis >= 6.0.3,<6.2.0",
-]
+install_requires = ["click", "spark-parser >= 1.8.9, < 1.9.2", "xdis >= 6.1.1, < 6.3.0"]
 
 license = "GPL3"
 mailing_list = "python-debugger@googlegroups.com"
-modname = "decompile_cfg"
+modname = "decompile-cfg"
 py_modules = None
 short_desc = "Python cross-version byte-code decompiler"
 web = "https://github.com/rocky/python-decompile-cfg/"
