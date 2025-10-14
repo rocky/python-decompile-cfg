e66ccecc70cb55e37afb3c25757d52220f492bf8
rocky <rb@dustyfeet.com>
2025-10-14 09:11:31 -0400

    Set up 3.8-to-3.10 branch packaging

--- setup.py
+++ setup.py
@@ -2,26 +2,44 @@
 
 """Setup script for the 'decompile-cfg' distribution."""
 
+import sys
 from setuptools import setup
+
+from __pkginfo__ import (
+    __version__,
+    author,
+    author_email,
+    classifiers,
+    entry_points,
+    install_requires,
+    license,
+    long_description,
+    modname,
+    py_modules,
+    short_desc,
+    web,
+    zip_safe,
+)
 
 import sys
 
 major = sys.version_info[0]
 minor = sys.version_info[1]
 
-if major != 3 or not minor >= 11:
+if major != 3 or not 8 <= minor < 10:
     sys.stderr.write(
-        "This installation medium is only for Python 3.11 and later. You are running Python %s.%s.\n"
+        "This installation medium is only for Python 3.8 to Python 3.10. You are running Python %s.%s.\n"
         % (major, minor)
     )
 
-    if major == 3 and 8 <= minor <= 10:
+    if major == 3 and minor > 11:
         sys.stderr.write(
-            "Please install using python-decompile-cfg_38-x.y.z.tar.gz from https://github.com/rocky/python-decoile-cfg/releases\n"
+            "Please install using the master branch or python-decompile-cfg-x.y.z.tar.gz from https://github.com/rocky/python-decompile-cfg/releases\n"
         )
         sys.exit(1)
-    else:
-        sys.stderr.write("Please install using an older release\n")
-        sys.exit(1)
+
+#!/usr/bin/env python
+"""Setup script for the 'decompyle3' distribution."""
+
 
 setup()
